d44aefdb5577bbb47f3e87ce716da767f7b0ab50
Nikas Praninskas <nikaspran@gmail.com>
2020-05-20 20:21:43 +0300

    Updates

---
+++ precache-manifest.b1d1562a5666a7ea2c382dd1697d1952.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7baca7e74ebe74d1d7ee926f03186e7",
+    "url": "/suggest-subreddit/index.html"
+  },
+  {
+    "revision": "d09267468140371ef5fa",
+    "url": "/suggest-subreddit/static/css/main.4a5eeca9.chunk.css"
+  },
+  {
+    "revision": "92a45e7d385dbca6e815",
+    "url": "/suggest-subreddit/static/js/2.a7050358.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/suggest-subreddit/static/js/2.a7050358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09267468140371ef5fa",
+    "url": "/suggest-subreddit/static/js/main.ad280297.chunk.js"
+  },
+  {
+    "revision": "1acb8b0dc0d46d414992",
+    "url": "/suggest-subreddit/static/js/runtime-main.b42a8ef1.js"
+  },
+  {
+    "revision": "54ac65fdbac1a7ae2d4bb91dcb8f035d",
+    "url": "/suggest-subreddit/static/media/cross.54ac65fd.svg"
+  },
+  {
+    "revision": "7c3e8212f2bd437bbafd8b57490bf9a2",
+    "url": "/suggest-subreddit/static/media/spinner.7c3e8212.svg"
+  }
+]);

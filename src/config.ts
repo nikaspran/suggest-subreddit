f5324d6e34614a8a37844f459d5e60f90088aa14
Nikas Praninskas <nikaspran@gmail.com>
2020-05-20 21:39:40 +0300

    Add production config

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ const dev = {
 };
 
 const prod = {
-  REDDIT_CLIENT_ID: 'ocEV8buEyuz35w',
-  REDDIT_REDIRECT_URI: 'http://localhost:3000',
+  REDDIT_CLIENT_ID: 'ubcs049_cdvODQ',
+  REDDIT_REDIRECT_URI: 'https://nikaspran.github.io/suggest-subreddit',
 };
 
 export default process.env.NODE_ENV === 'production' ? prod : dev;
